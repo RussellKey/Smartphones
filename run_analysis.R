50c13cec47448367d8aa4b86b7e6ceab8d40f8c3
RussellKey <RussellKey@users.noreply.github.com>
2016-02-24 18:02:09 -0800

    Create tidy data set

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(reshape2)
 
 setwd("C:\\Working\\UCI HAR Dataset\\merge")
 
@@ -20,16 +21,16 @@ X_test = read.table("X_test.txt", header=FALSE)
 
 # 1. Merges the training and the test sets to create one data set.
 testset <- cbind(subject_test, y_test, X_test)
-colnames(testset) <- c("Subject", "Activity")
+colnames(testset) <- c("Subject", "Activity", as.character(features[,2]))
 
 trainset <- cbind(subject_train, y_train, X_train)
-colnames(trainset) <- c("Subject", "Activity")
+colnames(trainset) <- c("Subject", "Activity", as.character(features[,2]))
 
 SmartPhones <- dplyr::union(testset, trainset)
 
 # 4. Appropriately labels the data set with descriptive variable names. 
 
-colnames(SmartPhones) <- c("Subject", "act", as.character(features[,2]))
+# colnames(SmartPhones) <- c("Subject", "act", as.character(features[,2]))
 
 valid_column_names <- make.names(names=names(SmartPhones), unique=TRUE, allow_ = TRUE)
 colnames(SmartPhones) <- valid_column_names
@@ -42,17 +43,22 @@ colnames(SmartPhones) <- valid_column_names
 # mean = 56
 # angle = 10(-)
 # + 2 = 81
-SP <- select(SmartPhones, Subject, act, contains("mean"), contains("std"), -contains("angle"))
-names(SP)
+SP <- select(SmartPhones, Subject, Activity, contains("mean"), contains("std"), -contains("angle"))
+# names(SP)
 
 # 3. Uses descriptive activity names to name the activities in the data set
-SP_ActivityNames <- inner_join(SP, activity_lables, by = c("act" = "id"))
-SP_ActivityNames <- select(SP_ActivityNames, -act)
+SP_ActivityNames <- inner_join(SP, activity_lables, by = c("Activity" = "id"))
+SP_ActivityNames <- select(SP_ActivityNames, -Activity)
 SP_ActivityNames <- select(SP_ActivityNames, Subject, activity, everything())
 
 # 5. From the data set in step 4, creates a second, 
 # independent tidy data set with the average of each variable for each activity 
 # and each subject.
 
+# melt
+SmartPhonesMelt <- melt(SP_ActivityNames, id=c("Subject","activity"), measure.vars=c(as.character(names(SP_ActivityNames[,3:ncol(SP_ActivityNames)]))))
+
+# dcast - tidy data set
+SmartPhonesDcast <- dcast(SmartPhonesMelt, Subject + activity ~ variable, mean)
 
                                
