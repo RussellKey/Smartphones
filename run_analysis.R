c9e07452e76e42301e5fb793605e71cf98fcf1aa
RussellKey <RussellKey@users.noreply.github.com>
2016-02-25 18:04:25 -0800

    Added comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,33 @@
 library(dplyr)
 library(reshape2)
 
-setwd("C:\\Working\\UCI HAR Dataset\\merge")
-
-#features
+#Read in features (processed 3-axial raw signals)
 features <- read.table("features.txt", header=FALSE)
-#activity labels
+#Ids and Labels of activities performed
 activity_lables <- read.table("activity_labels.txt", header=FALSE)
 colnames(activity_lables) <- c("id", "activity")
 
-#Subjects
+#Read in Subjects
 subject_train = read.table("subject_train.txt", header=FALSE)
 subject_test = read.table("subject_test.txt", header=FALSE)
-#Activity
+
+#Read in Activity
 y_train = read.table("y_train.txt", header=FALSE)
 y_test = read.table("y_test.txt", header=FALSE)
-#Data
+
+#Rading Data for Activities
 X_train = read.table("X_train.txt", header=FALSE)
 X_test = read.table("X_test.txt", header=FALSE)
 
 # 1. Merges the training and the test sets to create one data set.
+# Adds Subject and Activity column names
 testset <- cbind(subject_test, y_test, X_test)
 colnames(testset) <- c("Subject", "Activity", as.character(features[,2]))
 
 trainset <- cbind(subject_train, y_train, X_train)
 colnames(trainset) <- c("Subject", "Activity", as.character(features[,2]))
 
+# Combines the test and train datasets
 SmartPhones <- dplyr::union(testset, trainset)
 
 # 4. Appropriately labels the data set with descriptive variable names. 
@@ -38,7 +40,6 @@ colnames(SmartPhones) <- valid_column_names
 
 SP <- select(SmartPhones, Subject, Activity, contains("mean"), contains("std"), -contains("angle"))
 
-
 # 3. Uses descriptive activity names to name the activities in the data set
 SP_ActivityNames <- inner_join(SP, activity_lables, by = c("Activity" = "id"))
 SP_ActivityNames <- select(SP_ActivityNames, -Activity)
@@ -49,7 +50,6 @@ SP_ActivityNames <- select(SP_ActivityNames, Subject, activity, everything())
 # and each subject.
 
 # melt
-#SmartPhonesMelt <- melt(SP_ActivityNames, id=c("Subject","activity"), measure.vars=c(as.character(names(SP_ActivityNames[,3:ncol(SP_ActivityNames)]))))
 SmartPhonesMelt <- melt(SP_ActivityNames, id=c("Subject","activity"))
 
 # dcast - tidy data set
